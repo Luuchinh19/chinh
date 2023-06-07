1c45842f7df77dd18f9106463b8ab8d8cfeabf58
Luuchinh19 <133142337+Luuchinh19@users.noreply.github.com>
2023-06-07 13:56:39 +0700

    chinhchinh

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Page</title>
+</head>
+<body>
+    <h1>Welcome to my PHP Page</h1>
+
+    <?php
+    // Một đoạn mã PHP đơn giản
+    $message = "Hello, World!";
+    echo $message;
+    ?>
+
+    <p>This is a paragraph in HTML.</p>
+</body>
+</html>
